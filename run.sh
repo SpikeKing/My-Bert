6eb662c41921e3ad1a604faa64a91277965bb809
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-12-08 16:40:00 +0800

    update

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 bash
 
-export DATA_DIR=/media/ganjinzero/Code/bert/data/
-export BERT_BASE_DIR=/media/ganjinzero/Code/bert/chinese_L-12_H-768_A-12
+export DATA_DIR=./data/toutiao_dataset
+export BERT_BASE_DIR=./data/chinese_L-12_H-768_A-12
 
 python3 run_classifier.py \
-   --task_name=mytask \
-   --do_train=true \
-   --do_eval=true \
-   --data_dir=$DATA_DIR/ \
-   --vocab_file=$BERT_BASE_DIR/vocab.txt \
-   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
-   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
-   --max_seq_length=128 \
-   --train_batch_size=32 \
-   --learning_rate=2e-5 \
-   --num_train_epochs=3.0 \
-   --output_dir=./data/output_models/
+  --task_name=mytask \
+  --do_train=true \
+  --do_eval=true \
+  --data_dir=$DATA_DIR/ \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --max_seq_length=128 \
+  --train_batch_size=32 \
+  --learning_rate=2e-5 \
+  --num_train_epochs=3.0 \
+  --output_dir=./data/output_models/
--- run_classifier.py
+++ run_classifier.py
@@ -780,6 +780,58 @@ def convert_examples_to_features(examples, label_list, max_seq_length,
   return features
 
 
+class MyTaskProcessor(DataProcessor):
+    """
+    Processor for the News data set (GLUE version).
+    """
+
+    def __init__(self):
+        self.labels = [
+            "news_agriculture",
+            "news_car",
+            "news_culture",
+            "news_edu",
+            "news_entertainment",
+            "news_finance",
+            "news_game",
+            "news_house",
+            "news_military",
+            "news_sports",
+            "news_story",
+            "news_tech",
+            "news_travel",
+            "news_world",
+            "stock",
+        ]
+
+    def get_train_examples(self, data_dir):
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+    def get_test_examples(self, data_dir):
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+
+    def get_labels(self):
+        return self.labels
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+
+        examples = []
+
+        for (i, line) in enumerate(lines):
+            guid = "%s-%s" % (set_type, i)
+            text_a = tokenization.convert_to_unicode(line[1])
+            label = tokenization.convert_to_unicode(line[0])
+            examples.append(InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+
+        return examples
+
 def main(_):
   tf.logging.set_verbosity(tf.logging.INFO)
 
